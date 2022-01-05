1bab6ae0cc998b8f0ec298eb8ffe0e16e828a3f0
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-01-05 13:43:08 +0530

    to compute sum, mean and product

---
+++ Q10.r
@@ -0,0 +1,9 @@
+{
+  pumm_261 = c(1.3, 2.5, 3.8 )
+  print("Sum of the vector:")
+  print(sum(pumm_261))
+  print("Mean of the vector:")
+  print(mean(pumm_261))
+  print("Product of the vector:")
+  print(prod(pumm_261))
+}
