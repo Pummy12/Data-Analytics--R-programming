48f747944496b6370b257b83ced77cabc8c53806
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-01-05 13:40:26 +0530

    create 3 vectors numeric data, character data & logical data

---
+++ Q9.r
@@ -0,0 +1,11 @@
+{
+  pummy_261a = c(1,2,5)
+  pummy_261b = c("RED", "GREEN", "WHITE")
+  pummy_261c = c(TRUE,FALSE,TRUE)
+  print(pummy_261a)
+  cat("type is :",class(pummy_261a))
+  print(pummy_261b)
+  cat("type is :",class(pummy_261b))
+  print(pummy_261c)
+  cat("type is :",class(pummy_261c))
+}
