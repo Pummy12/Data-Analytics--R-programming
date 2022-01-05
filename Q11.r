1ba71c646f10020abe4c41992d695bc58645f9bd
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-01-05 13:43:55 +0530

    To calculate his gross salary

---
+++ Q11.r
@@ -0,0 +1,8 @@
+{
+  pumm_261 <- as.integer(readline("ENTER THE BASIC SALARY OF RAMESH:"))
+  pumm_261a <- 0.40*pumm_261
+  pumm_261h <- 0.20*pumm_261
+  pumm_261gs <- (pumm_261+pumm_261a+pumm_261h)
+  print("Gross Salary of Ramesh")
+  print(pumm_261gs)
+}
