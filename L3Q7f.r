19a37534bd631e2f12615098676a961bc2994b25
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-01-14 00:16:34 +0530

    retrieve the elements of 2nd 	row of 3rd matrix.

---
+++ L3Q7f.r
@@ -0,0 +1,14 @@
+{
+  # R program to illustrate
+  # rowSums() function
+  
+  # Initializing a matrix
+  pumm_261 <- matrix(rep(1:9), 3, 3)
+  
+  # Printing Matrix
+  print(pumm_261)
+  
+  # Calling the rowSums() function
+  rowSums(pumm_261)
+  
+}
