5cf93d8fcf33f2b92c6c0593dc9db51a3ba1fe08
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-01-14 00:11:31 +0530

    List having vector, matrix and a list & display only the 2nd data set of the list.

---
+++ L3Q4.r
@@ -0,0 +1,6 @@
+{
+  list_data <- list(c("Red","Green","Black"), matrix(c(1,3,5,7,9,11), nrow = 2),
+                    list("Java Script", "JAVA", "NODE JS"))
+  print("List:")
+  print(list_data)
+}
