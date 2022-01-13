b4f2d726eee631e79ea8b3fdd82dded39d04f310
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-01-14 00:16:01 +0530

    to check the given matrix is symmetric matrix or not

---
+++ L3Q7e.r
@@ -0,0 +1,10 @@
+{
+  # a matrix is symmetric
+  # Creating a diagonal matrix
+  pumm_261 <- diag(3)
+  # Creating a matrix
+  pumm_261a <- matrix(c(1, 2, 2, 3), 2)
+  # Calling isSymmetric() function
+  isSymmetric(pumm_261)
+  isSymmetric(pumm_261a)
+}
