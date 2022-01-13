46c7fd265de8eb7d3f7f766853ced6aebc89dc75
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-01-14 00:12:45 +0530

    merge two lists and display that

---
+++ L3Q6.r
@@ -0,0 +1,11 @@
+{
+  # Create two lists.
+  list1 <- list(1,2,3,4,5)
+  list2 <- list("JAN","FEB","MAR", "APR", "MAY")
+  
+  # Merge the two lists.
+  merged.list <- c(list1,list2)
+  
+  # Print the merged list.
+  print(merged.list)
+}
