3b3a2eb58372a728f1df2ee421b7050d58e62e5b
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-01-22 00:39:14 +0530

    find out the biggest one

---
+++ L4Q4.r
@@ -0,0 +1,8 @@
+{
+  pumm_261 <- as.integer(readline(prompt = "Enter a number1: "))
+  pumm_261a <- as.integer(readline(prompt = "Enter a number2: "))
+  if(pumm_261>pumm_261a)
+    print("Number 1 is greater")
+  else
+    print("Number 2 is greater")
+}
