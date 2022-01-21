9b0e73a92f0c25b3af35fbfb803048e156ad85f9
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-01-22 00:45:22 +0530

    sum the series S=1+(1+2)+(1+2+3)+...+(1+2+3+...+n)

---
+++ L4Q14.r
@@ -0,0 +1,17 @@
+{
+  num = as.integer(readline(prompt="Enter a number: "))
+   sum = 0
+   i = 1
+   j = 1
+  while ( i <= num && j <= i ){
+      sum =sum+j
+      i=i+1
+      j=j+1
+    }
+   print(paste(sum, " sum of series"))
+}
+
+
+
+
+
