92717b91e0d35c8d14485a0fdcce2aba02d2e39d
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-02-02 22:32:29 +0530

    search a specific element from vector

---
+++ L5Q7.r
@@ -0,0 +1,12 @@
+{
+  vec <- c('a','g','h','i')
+  
+  # check for presence of element 'a'
+  print ("Element 'a' presence in vector")
+  any('a'==vec)
+  
+  # check for presence of element 'y'
+  print ("Element 'y' presence in vector")
+  any('y'==vec)
+  
+}
