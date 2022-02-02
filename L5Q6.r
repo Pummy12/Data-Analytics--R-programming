e629618370f2e63523081f31d8bc3daedca3256e
Pummy Jha <64890582+Pummy12@users.noreply.github.com>
2022-02-02 22:31:52 +0530

    minimum value and maximum value, evaluate the sum of all the elements

---
+++ L5Q6.r
@@ -0,0 +1,10 @@
+{
+  pumm_261 = c(10, 20, 30, 40, 50, 60)
+  print('Original vector:')
+  print(pumm_261)   
+  print(paste("Maximum value of the said vector:",max(pumm_261)))
+  print(paste("Minimum value of the said vector:",min(pumm_261)))
+  print(paste("SUM of the said vector:",sum(pumm_261)))
+  
+  
+}
